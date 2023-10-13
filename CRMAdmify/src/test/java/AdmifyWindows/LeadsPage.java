aa986559466660737f654f0c692628e56ae4d400
Punit Sahu <punit.sahu@eulogik.com>
2023-10-13 17:27:48 +0530

    third commit

--- CRMAdmify/src/test/java/AdmifyWindows/LeadsPage.java
+++ CRMAdmify/src/test/java/AdmifyWindows/LeadsPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class LeadsPage extends LoginPage {
+//public class LeadsPage extends Dashboard {
 
 	@Test
 	public void AddLeads() throws InterruptedException {
@@ -86,8 +87,196 @@ public class LeadsPage extends LoginPage {
 		
 		List <WebElement> classes =driver.findElements(By.xpath("/html/body/div[4]/div[3]"));
 		
+		for (WebElement element : classes) {
+			System.out.println(element.getText());
+			
+			//element.getAccessibleName();
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			
+			System.out.println("classs selected");	
+		}
+		// click on country dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[9]/div/div/div")).click();
+		//Select country from dropdown
+		List <WebElement> country = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : country) {
+			System.out.println(element.getText());
+		if(element.getText().equals(String.valueOf(2)));
+		element.click();
+		System.out.println("country selected");
 		
 		}
+		
+		//click on state dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[10]/div/div/div")).click();
+		//selecting state from dropdown
+		List <WebElement> state =driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element :state) {
+			System.out.println(element.getText());
+		if(element.getText().equals(String.valueOf(3)));
+			element.click();
+			System.out.println("state selected");
+		}
+		
+		//click on city input box
+		WebElement city = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[11]/div/div/input"));
+		city.click();
+		city.sendKeys("bhopal");
+		
+		//click on Referrer dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[12]/div/div/div")).click();
+		//selecting value from dropdown
+		List <WebElement> State = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : State) {
+			System.out.println(element.getText());
+		if(element.getText().equals(String.valueOf(3)));
+			element.click();
+			System.out.println("Referrer selected");
+		}
+		//click on Address input box
+		WebElement address = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[13]/div/div/input"));
+		address.click();
+		address.sendKeys("Eulogik zone 2 mp nagar bhopal");
+		
+		//click on course dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[14]/div/div/div")).click();
+		//click on course dropdown
+		List <WebElement> course = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : course) {
+			System.out.println(element.getText());
+		if(element.getText().equals(String.valueOf(3)));
+			element.click();
+			System.out.println("Course selected");
+		}
+		
+		//click on previous class marks%
+		WebElement previousMarks = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[15]/div/div/input"));
+		previousMarks.click();
+		previousMarks.sendKeys("98.00");
+		
+		//click on zipcode input box
+		WebElement zipcode = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[16]/div/div/input"));
+		zipcode.click();
+		zipcode.sendKeys("223344");
+		
+		//click on Board dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[17]/div/div/div")).click();
+		//select value from dropdown
+		List <WebElement> board = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : board) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			System.out.println("board selected");
+		}
+		
+		//click on medium dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[18]/div/div/div")).click();
+		//click on medium dropdown
+		List <WebElement> medium = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : medium) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			System.out.println("medium selected");
+		}
+		
+		//click on stream dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[19]/div/div/div")).click();
+		//click on stream dropdown
+		List <WebElement> stream = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : stream) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			System.out.println("stream selected");
+		}
+		
+		//click on school dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[20]/div/div/div")).click();
+		//click on school dropdown
+		List <WebElement> school = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : school) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(3)));
+			element.click();
+			System.out.println("School selected");
+		}
+		
+		//click on source dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[21]/div/div/div")).click();
+		//click on source dropdown
+		List <WebElement> source = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : source) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			System.out.println("stream selected");
+		}
+		
+		//click on Session dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[22]/div/div/div")).click();
+		//click on Session drodown
+		List <WebElement> Session = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : Session) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(3)));
+			element.click();
+			System.out.println("session selected");
+		}
+		
+		//click on father name input box
+		WebElement fatherName = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[23]/div/div/input"));
+		fatherName.click();
+		fatherName.sendKeys("Father");
+		
+		//click on fathers occupation
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[24]/div/div/div")).click();
+		//click on fathers occupation dropdown
+		List <WebElement> FatherOccupation = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : FatherOccupation) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(3)));
+			element.click();
+			System.out.println("farther occupation selected");
+		}
+		//click on Mothers name
+		WebElement mothername = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[25]/div/div/input"));
+		mothername.click();
+		mothername.sendKeys("Mother");
+		
+		//click on mothers occupation dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[26]/div/div/div")).click();
+		//click on Mothers occupation dropdown
+		List <WebElement> motherOccupation = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : motherOccupation) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			System.out.println("mothers occupation selected");	
+		}
+		
+		//click on parents contact no
+		WebElement ParentContact = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[27]/div/div/input"));
+		ParentContact.click();
+		ParentContact.sendKeys("4455678998");
+		
+		//click on groups dropdown
+		driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/div/div/div[28]/div/div/div")).click();
+		//click on groups dropdown
+		List <WebElement> group = driver.findElements(By.xpath("/html/body/div[4]/div[3]/ul"));
+		for(WebElement element : group) {
+			System.out.println(element.getText());
+			if(element.getText().equals(String.valueOf(4)));
+			element.click();
+			System.out.println("groups selected");
+		}
+		
+		
+		
+		
+		
 	}
+}
 	
-
